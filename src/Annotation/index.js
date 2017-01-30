e764ec6e9b67b098ab097cd8537be2f532eb3c2a
Andrew Mouat <plz_nosparem_thnx@hotmail.com>
2017-01-31 01:08:11 +1100

    started on transformation of Geometry string

--- src/Annotation/index.js
+++ src/Annotation/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 import rgba  from "../__lib__/unit_to_rgba"
+// Many thanks to this commented out file, but it caused compilation errors. :(
+//import transform_point from "../__lib__/xform_point"
 
 /*
 	LIGHT BULB
@@ -12,8 +14,67 @@ import rgba  from "../__lib__/unit_to_rgba"
 	Geometry: "M100,100 L400,100 L400,400 L100,400 Z" with Matrix: [2,0,0,1,0,0]
 	for example. Bring on the alcohol and let's get it happening!
  */
+/*
+	A little regex and xform_point from src/__lib__ directory should do the job!
+	Unfortunately, while a little alcohol would help I have none available. :'(
+*/
 
-export default props => <path d={props.Geometry} stroke={rgba(props.StrokeColor)}
-	strokeWidth={props.StrokeThickness} fill={rgba(props.FillColor)}
-	transform={`matrix(${ props.Matrix.join(",") })`}
+var transform_point = function([x, y], [a, d, b, e, c, f]){
+	return {x: a*x+b*y+c, y: d*x+e*y+f};
+}
+
+/*
+	This method takes the geometry as 
+	M or m followed immediately by 2 comma separated co-ordinates
+	L or l followed immediately by 2 comma separated co-ordinates
+	H or h followed immediately by 1 ordinate
+	V or v followed immediately by 1 ordinate
+	Z followed by nothing (ordinates or otherwise)
+	and replaces all ordinates in the string with their transformed ordinates.
+ */
+// example of Geometry "M100,100 L400,100 H400 V400 Z"
+var convertGeometry = function(Geometry, Matrix){
+
+console.log('Geometry = ' + Geometry);
+
+	var geom = '';
+	var index = 0;
+
+	while(index < Geometry.length){
+		var nextCmd = Geometry.substr(index,1);
+		geom += nextCmd;
+
+		switch(nextCmd){
+			case 'M':
+			case 'm':
+			case 'L':
+			case 'l':
+
+				index += 9;
+				break;
+			case 'H':
+			case 'h':
+
+				index += 5;
+				break;
+			case 'V':
+			case 'v':
+
+				index += 5;
+				break;
+			case 'Z':
+			case 'z':
+				index += 2;
+				break;
+			default:
+				console.log('enslave the humans!');
+		}
+	}
+console.log('geom = ' + geom);
+	return geom;
+}
+
+export default props => <path d={convertGeometry(props.Geometry,props.Matrix)} 
+	stroke={rgba(props.StrokeColor)} strokeWidth={props.StrokeThickness} 
+	fill={rgba(props.FillColor)}
   	/>
--- src/__lib__/compose.js
+++ src/__lib__/compose.js
@@ -1,5 +1,6 @@
 import multiply from "./multiply"
 
+// recursively multiply, until there's nothing left to multiply by!
 var compose = (first, ...mxs) => {
   if (!mxs.length) return first
   var [next, ...rest] = mxs
